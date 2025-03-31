7bc346f9cf02e4877cd664d135630827eb737afa
Dmitry Razumov <dima.razumov.04.work@gmail.com>
2025-03-31 14:26:24 +0300

    feat: add category recipes content

--- src/components/sections/CategoryContent.tsx
+++ src/components/sections/CategoryContent.tsx
@@ -3,17 +3,22 @@
 import { sortRecipes } from '@/data';
 import { ICategory, ISelectOption } from '@/utils/interfaces';
 
+import { LoadMoreRecipes, RecipesCardsList } from '../blocks';
 import { Select } from '../ui/selects/Select';
+
+const RECIPES_PER_PAGE: number = 16;
 
 export const CategoryContent = ({ recipes, description, name }: Omit<ICategory, 'slug' | 'fullImage'>) => {
   const hasRecipes: boolean = recipes.length > 0;
+  const initialRecipes = recipes.slice(0, RECIPES_PER_PAGE);
+  const remainingRecipes = recipes.slice(RECIPES_PER_PAGE);
 
   const onChangeSelect = (value: ISelectOption) => alert(`Выбрана сортировка ${value.value}`);
 
   return (
-    <section>
+    <section className="mb-24">
       <div className="custom-container">
-        <div className="flex justify-between items-end">
+        <div className="flex justify-between items-end mb-12">
           <div className="flex flex-col gap-y-2 max-w-2xl">
             <div className="flex items-end gap-x-4">
               <h1 className="font-unbounded text-4xl">{name}</h1>
@@ -23,6 +28,8 @@ export const CategoryContent = ({ recipes, description, name }: Omit<ICategory, 
           </div>
           <Select onChange={onChangeSelect} value={sortRecipes[0]} options={sortRecipes} className="w-52" />
         </div>
+        <RecipesCardsList cards={initialRecipes} nothingMsg={'Рецептов данной категории нет'} />
+        <LoadMoreRecipes remainingRecipes={remainingRecipes} perPage={RECIPES_PER_PAGE} />
       </div>
     </section>
   );
--- src/components/sections/CategoryHeaderImage.tsx
+++ src/components/sections/CategoryHeaderImage.tsx
@@ -7,7 +7,7 @@ interface ICategoryHeaderImageProps {
 export const CategoryHeaderImage = ({ img }: ICategoryHeaderImageProps) => {
   return (
     <section className="w-full h-72 relative mb-24">
-      <Image src={img} alt="" fill style={{ objectFit: 'cover' }} />
+      <Image src={img} alt="" fill style={{ objectFit: 'cover' }} priority fetchPriority="high" />
     </section>
   );
 };
