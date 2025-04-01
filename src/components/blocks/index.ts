5ba871427323b2e14f16ad48f6a38fb9198243d7
Dmitry Razumov <dima.razumov.04.work@gmail.com>
2025-04-01 15:41:31 +0300

    feat: add blocks instruction and ingredients

--- src/components/blocks/index.ts
+++ src/components/blocks/index.ts
@@ -3,15 +3,6 @@ import { EmailNewsletterForm } from './EmailNewsletterForm';
 import { LoadMoreCollections } from './LoadMoreCollections';
 import { LoadMoreRecipes } from './LoadMoreRecipes';
 import { MainSliderBtns } from './MainSliderBtns';
-import { RecipeRating } from './RecipeRating';
 import { TitleSectionBlock } from './TitleSectionBlock';
 
-export {
-  CardsItems,
-  EmailNewsletterForm,
-  LoadMoreCollections,
-  LoadMoreRecipes,
-  MainSliderBtns,
-  RecipeRating,
-  TitleSectionBlock,
-};
+export { CardsItems, EmailNewsletterForm, LoadMoreCollections, LoadMoreRecipes, MainSliderBtns, TitleSectionBlock };
---
+++ src/components/blocks/recipe/index.ts
@@ -0,0 +1,5 @@
+import { RecipeIngredients } from './RecipeIngredients';
+import { RecipeInstruction } from './RecipeInstruction';
+import { RecipeRating } from './RecipeRating';
+
+export { RecipeIngredients, RecipeInstruction, RecipeRating };
