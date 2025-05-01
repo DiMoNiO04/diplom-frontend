ea34d830d674bc9535e41c11e3acace29d6e0b37
Dmitry Razumov <dima.razumov.04.work@gmail.com>
2025-05-01 15:01:14 +0300

    refactor: delete static categories

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,7 +1,6 @@
-import { categoriesData } from './categories';
 import { menuFooterData } from './menuFooter';
 import { menuHeaderData } from './menuHeader';
 import { menuProfileData } from './profileLinks';
 import { ESortRecipes, sortRecipes } from './sortRecipes';
 
-export { categoriesData, ESortRecipes, menuFooterData, menuHeaderData, menuProfileData, sortRecipes };
+export { ESortRecipes, menuFooterData, menuHeaderData, menuProfileData, sortRecipes };
